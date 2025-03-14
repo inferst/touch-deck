3d324c2414d9009f2d8e56de9480e5e196bee7a4
Mikhail Morozov <inferst@gmail.com>
2025-03-14 20:18:22 +0300

    feat: serve deck app static in tauri app

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,3 +1,9 @@
+use std::{net::SocketAddr, path::Path};
+
+use axum::Router;
+use tauri::{Manager, path::BaseDirectory};
+use tower_http::services::ServeDir;
+
 // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
 #[tauri::command]
 fn greet(name: &str) -> String {
@@ -7,8 +13,24 @@ fn greet(name: &str) -> String {
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
+        .setup(|app| {
+            let deck_path = app.path().resolve("deck", BaseDirectory::Resource)?;
+            tauri::async_runtime::spawn(serve(using_serve_dir(&deck_path), 3000));
+            Ok(())
+        })
+        .plugin(tauri_plugin_fs::init())
         .plugin(tauri_plugin_opener::init())
         .invoke_handler(tauri::generate_handler![greet])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
+}
+
+fn using_serve_dir(path: &Path) -> Router {
+    Router::new().nest_service("/deck", ServeDir::new(path))
+}
+
+async fn serve(app: Router, port: u16) {
+    let addr = SocketAddr::from(([0, 0, 0, 0], port));
+    let listener = tokio::net::TcpListener::bind(addr).await.unwrap();
+    axum::serve(listener, app).await.unwrap();
 }
