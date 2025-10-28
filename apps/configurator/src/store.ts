707cf0269ee2cbe8d477313a171c9fa3e374f2ba
Mikhail Morozov <inferst@gmail.com>
2025-10-28 20:36:24 +0300

    feat: local store

--- apps/configurator/src/store.ts
+++ apps/configurator/src/store.ts
@@ -1,3 +1,51 @@
+import { isTauri } from "@tauri-apps/api/core";
 import { LazyStore } from "@tauri-apps/plugin-store";
 
-export const store = new LazyStore("settings.json");
+class LocalStoreAdapter {
+  constructor(private readonly store: Storage) {}
+
+  get<T>(key: string): Promise<T | undefined> {
+    const item = this.store.getItem(key);
+
+    if (item) {
+      return Promise.resolve(JSON.parse(item) as T);
+    }
+
+    return Promise.resolve(undefined);
+  }
+
+  set(key: string, value: unknown): Promise<void> {
+    return Promise.resolve(this.store.setItem(key, JSON.stringify(value)));
+  }
+
+  save(): Promise<void> {
+    return Promise.resolve();
+  }
+}
+
+class TauriStoreAdapter {
+  constructor(private readonly store: LazyStore) {}
+
+  get<T>(key: string): Promise<T | undefined> {
+    return this.store.get<T>(key);
+  }
+
+  set(key: string, value: unknown): Promise<void> {
+    return this.store.set(key, value);
+  }
+
+  save(): Promise<void> {
+    return this.store.save();
+  }
+}
+
+function createStore(isTauri: boolean) {
+  if (isTauri) {
+    const store = new LazyStore("settings.json");
+    return new TauriStoreAdapter(store);
+  } else {
+    return new LocalStoreAdapter(localStorage);
+  }
+}
+
+export const store = createStore(isTauri());
